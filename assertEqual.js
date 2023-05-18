c4c1ab82753b9031fbb5ec67c1e4d8f8ae3bfef1
Devvaru <syeraru@gmail.com>
2023-05-18 16:08:01 -0600

    Create function to improve on console.assert

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ Assertation Passed: " + actual + ' === ' + expected);
+  } else {
+    console.log("🛑 Assertation Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Mango", "Mango");
+assertEqual(2, 5);
