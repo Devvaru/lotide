eeba647b6b00d811b21f223bc3220519c8024ba1
Devvaru <syeraru@gmail.com>
2023-05-21 19:06:50 -0600

    Create assertion function for comparing arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function(arr1, arr2) {
+  let allTrue;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } else {
+      allTrue = true;
+    }
+  }
+  return allTrue;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertation Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑 Assertation Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3,], [1, 2, 3]);
+assertArraysEqual([1, 2, 3,], [3, 2, 4]);
