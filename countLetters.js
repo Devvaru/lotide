cc81745839e57b39d927148a69d5f9d4a4ffdaf2
Devvaru <syeraru@gmail.com>
2023-05-23 18:51:14 -0600

    Create function to count how many times a letter appears in a sentence

---
+++ countLetters.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertation Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const string = "Once upon a time";
+const countLetters = function(sentence) {
+  // return count of each letter in sentence
+  let result = {};
+  sentence = sentence.toLowerCase();
+  for (let letter of sentence) {
+    if (letter !== " ") {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+const results = countLetters(string, {
+  "o": 2,
+  "n": 2,
+  "u": 1,
+  "b": 0,
+  "p": 1
+});
+
+console.log(countLetters(string));
+
+assertEqual(results["n"], 2);
+assertEqual(results["u"], 1);
+assertEqual(results["p"], 1);
