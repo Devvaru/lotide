00e73185766aeaf6c429cfe79fdbbe0f64cf2a16
Devvaru <syeraru@gmail.com>
2023-05-25 19:55:00 -0600

    Create function that returns the key of an object based on the value

---
+++ findKeyByValue.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertation Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = (obj, val) => {
+  for (let item in obj) {
+    if (obj[item] === val) {
+      return item;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+// Alternate solution via Larry bot
+
+// const findKeyByValue = (obj, val) => {
+//   const keys = Object.keys(obj);
+//   for(let i = 0; i < keys.length; i++) {
+//     if(obj[keys[i]] === val) {
+//       return keys[i];
+//     }
+//   }
+// };
