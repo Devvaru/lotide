ea22afa8bfa4fe1ada4e412756b03909f9978f66
Devvaru <syeraru@gmail.com>
2023-06-08 15:58:55 -0600

    Implement Chai and Mocha testing for findKeyByValue

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertation Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertation Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKeyByValue = (obj, val) => {
   for (let item in obj) {
     if (obj[item] === val) {
@@ -13,17 +5,6 @@ const findKeyByValue = (obj, val) => {
     }
   }
 };
-
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire"
-};
-
-console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 
 module.exports = findKeyByValue;
 
