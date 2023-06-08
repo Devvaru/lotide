67990ec12a7da421ddbd10edb7fac96f73f5cbdb
Devvaru <syeraru@gmail.com>
2023-06-08 17:38:20 -0600

    Implement Mocha and Chai for eqArrays

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,13 +1,23 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+// const eqArrays = require('../eqArrays');
+
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+describe("#eqArrays", () => {
+  it("returns true for [1, 2, 3], [1, 2, 3]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+  it("returns false for [1, 2, 3], [3, 2, 1]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+  it("returns true for ['1', '2', '3'], ['1', '2', '3']", () => {
+    assert.strictEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
+  });
+  
+  it("returns false for ['1', '2', '3'], ['1', '2', 3]", () => {
+    assert.strictEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
+  });
+});
