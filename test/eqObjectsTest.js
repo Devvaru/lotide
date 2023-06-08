e550a6128e1e7ccc2b2d79ad95224ea714085223
Devvaru <syeraru@gmail.com>
2023-06-08 16:15:50 -0600

    Implement Chai and Mocha testing for eqObjects

---
+++ test/eqObjectsTest.js
@@ -0,0 +1,32 @@
+// const assertEqual = require('../assertEqual');
+// const eqArrays = require('../eqArrays');
+
+const assert = require('chai').assert;
+const eqObjects = require('../eqObjects');
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+
+describe("#eqObjects", () => {
+  it("returns true for shirtObject and anotherShirtObject", () => {
+    assert.deepEqual(eqObjects(shirtObject, anotherShirtObject), true);
+  });
+
+  it("returns false for shirtObject and longSleeveShirtObject", () => {
+    assert.deepEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
+  });
+
+  it("returns true for multiColorShirtObject and anotherMultiColorShirtObject", () => {
+    assert.deepEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
+  });
+  
+  it("returns false for multiColorShirtObject and longSleeveMultiColorShirtObject", () => {
+    assert.deepEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false);
+  });
+
+});
