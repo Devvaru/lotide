cff61b1429485d105189f34a7e74e07d485aac70
Devvaru <syeraru@gmail.com>
2023-06-10 02:50:15 -0600

    Remove outdated lines of code

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -7,14 +7,3 @@ const findKeyByValue = (obj, val) => {
 };
 
 module.exports = findKeyByValue;
-
-// Alternate solution via Larry bot
-
-// const findKeyByValue = (obj, val) => {
-//   const keys = Object.keys(obj);
-//   for(let i = 0; i < keys.length; i++) {
-//     if(obj[keys[i]] === val) {
-//       return keys[i];
-//     }
-//   }
-// };
